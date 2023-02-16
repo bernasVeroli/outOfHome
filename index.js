7c141786f99d2b99d23902c0f6734af742ff1493
bernasVeroli <125089414+bernasVeroli@users.noreply.github.com>
2023-02-16 19:12:48 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+const sFirst = document.getElementById("sFirst");
+const sSecond = document.getElementById("sSecond");
+const sThird = document.getElementById("sThird");
+const text = document.getElementById("text");
 
+const tFirst = 1;
+const tSecond = 2;
+const tThird = 3;
+
+var textResult = 0;
+function principal() {
+  switch (textResult) {
+  case 0 :
+    text.textContent = "";
+    break;
+  case 1 :
+    text.textContent = tFirst;
+    break;
+  }
+}
+sFirst.addEventListener("click", function(i = false){
+  i = !i;
+  alert(i);
+});
+principal();
